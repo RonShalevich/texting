2067bdc80488f07f8fd1c4122b4d39601edecaee
Ron Shalevich <ronshalevich@Rons-MacBook-Air.local>
2017-07-27 12:16:52 -0700

    hide auth token

--- text.py
+++ text.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "AC9d28751ba04a269f2e240c92e08e67ff"
 # Your Auth Token from twilio.com/console
-auth_token = "80c1a3137b672dc7a8886f22c8940877"
+auth_token = ""
 
 client = Client(account_sid, auth_token)
 
